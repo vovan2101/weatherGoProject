175d0c006e9f882bd65e4c23658cda35f287d6a0
Vlad Eliseykin <veliseykin2000@gmail.com>
2023-10-25 14:24:31 -0700

    second version of a program

--- main/main.go
+++ main/main.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	OPENWEATHER_API_KEY = "e466bef46d86e7b3c52399fa85c0e758"
-	OPENAI_API_KEY      = "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"
+	OPENAI_API_KEY      = "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"
 )
 
 type Message struct {
@@ -54,29 +54,58 @@ func userInput() {
 		fmt.Println("Error fetching weather data:", err)
 		return
 	}
-	response := formulateResponse(city, query, weatherData)
-	fmt.Println(response)
 
+	response, err := formulateResponseWithOpenAI(city, query, weatherData)
+	if err != nil {
+		fmt.Println("Error formulating response:", err)
+		return
+	}
+	fmt.Println(response)
 }
 
-func formulateResponse(city, query string, weatherData WeatherResponse) string {
+func formulateResponseWithOpenAI(city, query string, weatherData WeatherResponse) (string, error) {
 	tempCelsius := weatherData.Main.Temp - 273.15
 	weatherDescription := "Unknown"
 	if len(weatherData.Weather) > 0 {
 		weatherDescription = weatherData.Weather[0].Description
 	}
 
-	if strings.Contains(query, "rain") {
-		if strings.Contains(weatherDescription, "rain") {
-			return fmt.Sprintf("Yes, it's currently raining in %s. The weather is described as %s.", city, weatherDescription)
-		} else {
-			return fmt.Sprintf("No, it's not raining in %s right now. The weather is described as %s.", city, weatherDescription)
-		}
-	} else if strings.Contains(query, "temperature") || strings.Contains(query, "warm") {
-		return fmt.Sprintf("The temperature in %s is %.2f°C.", city, tempCelsius)
-	} else {
-		return fmt.Sprintf("The temperature in %s is %.2f°C, and the weather is described as %s.", city, tempCelsius, weatherDescription)
+	systemMessage := "You are a helpful and friendly assistant. Given the current weather data, please provide an informative and conversational response to the user's question."
+	userMessage := fmt.Sprintf("The temperature in %s is %.2f°C, and the weather is described as %s. Question: %s", city, tempCelsius, weatherDescription, query)
+
+	messages := []Message{
+		{Role: "system", Content: systemMessage},
+		{Role: "user", Content: userMessage},
 	}
+
+	payload := map[string]interface{}{
+		"model":    "gpt-4",
+		"messages": messages,
+	}
+	body, _ := json.Marshal(payload)
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", strings.NewReader(string(body)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+OPENAI_API_KEY)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	bodyResp, _ := io.ReadAll(resp.Body)
+
+	var response OpenAIResponse
+
+	json.Unmarshal(bodyResp, &response)
+
+	if len(response.Choices) > 0 && response.Choices[0].Message.Content != "" {
+		return response.Choices[0].Message.Content, nil
+	}
+	return "", fmt.Errorf("no content from openAI")
 }
 
 func interpretQueryWithOpenAI(query string) (string, error) {
@@ -107,7 +136,14 @@ func interpretQueryWithOpenAI(query string) (string, error) {
 	bodyResp, _ := io.ReadAll(resp.Body)
 
 	var response OpenAIResponse
+
+	fmt.Println("OpenAI Interpret Response:", string(bodyResp))
+
 	json.Unmarshal(bodyResp, &response)
+
+	if len(response.Choices) == 0 {
+		return "", fmt.Errorf("OpenAI API did not return expected choices")
+	}
 
 	if len(response.Choices) > 0 && response.Choices[0].Message.Content != "" {
 		return response.Choices[0].Message.Content, nil
--- main/main_test.go
+++ main/main_test.go
@@ -1,29 +1,60 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestFormulateResponse(t *testing.T) {
-	testWeatherData := WeatherResponse{
-		Main: struct {
-			Temp float64 `json:"temp"`
-		}{
-			Temp: 290.15, // ~17°C
-		},
-		Weather: []struct {
-			Description string `json:"description"`
-		}{
-			{
-				Description: "clear",
+// Проверяем, что функция interpretQueryWithOpenAI коректно находит город среди пользовательского запроса.
+func TestInterpretQueryWithOpenAI(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+
+		if req.Header.Get("Content-Type") != "application/json" {
+			t.Errorf("Expected Content-Type application/json, got %s", req.Header.Get("Content-Type"))
+		}
+		if req.Header.Get("Authorization") != "Bearer "+OPENAI_API_KEY {
+			t.Errorf("Expected Authorization Bearer %s, got %s", OPENAI_API_KEY, req.Header.Get("Authorization"))
+		}
+
+		response := OpenAIResponse{
+			Choices: []struct {
+				Message struct {
+					Content string `json:"content"`
+				} `json:"message"`
+			}{
+				{
+					Message: struct {
+						Content string `json:"content"`
+					}{
+						Content: "London",
+					},
+				},
 			},
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(response)
+	}))
+	defer server.Close()
+
+	query := "What's the weather like in London?"
+	expectedCity := "London"
+
+	oldDefaultClient := http.DefaultClient
+	http.DefaultClient = &http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
 		},
 	}
+	defer func() { http.DefaultClient = oldDefaultClient }()
 
-	got := formulateResponse("Paris", "What's the temperature in Paris right now?", testWeatherData)
-	expected := "The temperature in Paris is 17.00°C."
-
-	if got != expected {
-		t.Errorf("got %s; expected %s", got, expected)
+	city, err := interpretQueryWithOpenAI(query)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if city != expectedCity {
+		t.Errorf("Expected city '%s', got '%s'", expectedCity, city)
 	}
 }
